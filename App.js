20168c6667c9445dbf4074af9720d355ea29582e
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-07 21:12:09 +0100

    Why it freezes

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ApolloWrapper } from "./+components/ApolloWrapper";
-import { Markers } from "./+components/Markers";
+import { AuthorizationButtonManual } from "./+components/AuthorizationButtonManual";
+// import { Markers } from "./+components/Markers";
 
 export default function App() {
   return (
     <ApolloWrapper>
-      <Markers />
+      <AuthorizationButtonManual />
     </ApolloWrapper>
   );
 }
