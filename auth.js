b7cc57154a7e2a3d078d67f6641cf754e63c2406
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-07 11:05:16 +0100

    Additonals

--- auth.js
+++ auth.js
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 export const Auth0Context = React.createContext();
 export const useAuth0 = () => useContext(Auth0Context);
 
 export const Auth0Provider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState();
+
   return <>{children}</>;
 };
